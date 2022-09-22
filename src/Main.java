faa903834b690979ee877ba1ec2936b971032457
Sviirina <vozilkina@inbox.ru>
2022-09-22 21:55:29 +0700

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 6
+        System.out.println("Задание 6");
+        var boxerOne = 78.2;
+        var boxerTwo = 82.7;
+        System.out.println("Общий вес двух боксеров: " + (boxerOne + boxerTwo));
+        System.out.println("Разница в весе двух боксеров: " + (boxerTwo - boxerOne));
+
+        //Задание 7
+        System.out.println("Задание 7");
+        System.out.println("Разница в весе двух боксеров (Способ 1): " + (boxerTwo - boxerOne));
+        System.out.println("Разница в весе двух боксеров (Способ 2): " + (boxerTwo % boxerOne));
+
+        //Задание 7
+        System.out.println("Задание 8");
+        var totalHours = 640;
+        var hoursPerWorking = 8;
+        var totalEmployees = totalHours / hoursPerWorking;
+        System.out.println("Всего работников в компании - " + totalHours / hoursPerWorking + " человек");
+        totalEmployees = totalEmployees + 94;
+        totalHours = totalEmployees * hoursPerWorking;
+        System.out.println("Если в компании работает " + totalEmployees + " человека, то всего " + totalHours + " часов работы может быть поделено между сотрудниками");
+
+
+
+
     }
 }
